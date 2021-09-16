a8a69772244e2318cfb079abf7151bfa844d3bf6
unknown <yeasin.mohammod018@gmail.com>
2021-09-17 04:22:49 +0600

    css done

---
+++ myScript.js
@@ -0,0 +1,16 @@
+// let title = document.querySelectorAll(".title");
+
+// for(let i = 0; i < title.length; i++){
+//   title[i].addEventListener("click", function(){
+//     let result = this.nextElementSibling;
+//     result.classList.toggle("active");
+
+//      if(this.firstElementChild.innerText === "+"){
+//        this.firstElementChild.innerText = "✕"
+//        }
+//     else{
+//       this.firstElementChild.innerText = "+"
+//     }
+
+//   })
+// }
